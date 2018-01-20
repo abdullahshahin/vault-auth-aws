85a907e6ef775bbc6be9b45a6832de4ba10f0c15
Ricky Hussmann <ricky.hussmann@move.com>
2018-01-20 17:48:02 -0500

    Update tests to account for AWS credentials

--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,18 @@
 const index = require('./../index');
+
+let creds = {accessKeyId: '', secretAccessKey: ''};
+
 describe('Testing index file functions', () => {
     it('1) options must be json', () => {
         let vaultAuth = new index(configs(0));
-        let options = vaultAuth.getOptions();
+        let options = vaultAuth.getOptions(creds);
         let objectConstructor = {}.constructor;
         expect(options.constructor).toEqual(objectConstructor);
     });
 
     it('2) options must include at least url, followAllRedirects and body', () => {
         let vaultAuth = new index(configs(0));
-        let options = vaultAuth.getOptions();
+        let options = vaultAuth.getOptions(creds);
         expect(typeof options['url']).toEqual('string');
         expect(typeof options['followAllRedirects']).toEqual('boolean');
         expect(typeof options['body']).toEqual('string');
@@ -17,7 +20,7 @@ describe('Testing index file functions', () => {
 
     it('3) if certFilePath was there then options must include cert', () => {
         let vaultAuth = new index(configs(1));
-        let options = vaultAuth.getOptions();
+        let options = vaultAuth.getOptions(creds);
         expect(typeof options['url']).toEqual('string');
         expect(typeof options['followAllRedirects']).toEqual('boolean');
         expect(typeof options['body']).toEqual('string');
@@ -26,7 +29,7 @@ describe('Testing index file functions', () => {
 
     it('4) if sslRejectUnAuthorized was false then NODE_TLS_REJECT_UNAUTHORIZED must be 0', () => {
         let vaultAuth = new index(configs(2));
-        let options = vaultAuth.getOptions();
+        let options = vaultAuth.getOptions(creds);
         expect(process.env['NODE_TLS_REJECT_UNAUTHORIZED']).toEqual('0');
     });
 });
